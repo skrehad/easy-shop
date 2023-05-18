bd77860599c544f41eae092df2dc53a71a580641
rehadhasan <rehadhasan664@gmail.com>
2023-05-18 20:24:17 +0600

    added not found page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Page NotFound</div>;
+  return (
+    <div className="text-center">
+      <p className="text-[250px] font-mono font-bold">404</p>
+      <p className="text-2xl font-mono font-bold">Page Not Found</p>
+      <p className="font-mono mx-5 font-bold text-lg">
+        The page you are looking for doesn't exist. Go to{" "}
+        <Link to="/" className="text-[#ff3633] underline">
+          Home Page
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default NotFound;
