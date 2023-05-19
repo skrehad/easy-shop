7937d957743872b50573ad1d65ac10a7d400f279
rehadhasan <rehadhasan664@gmail.com>
2023-05-19 14:29:16 +0600

    added multipul components in home section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,15 +2,27 @@ import React from "react";
 import useTitle from "../../Shared/TitleChange/TitleChange";
 import PageAnalysis from "./PageAnalysis/PageAnalysis";
 import HomeContact from "./HomeContact/HomeContact";
+import MainSection from "./MainSection/MainSection";
+import HomeBlog from "./HomeBlog/HomeBlog";
+import Services from "./Services/Services";
+import Carousel from "./Carousel/Carousel";
+import OfferSection from "./OfferSection/OfferSection";
+import TopSellingProducts from "./TopSellingProducts/TopSellingProducts";
+import HomeReview from "./HomeReview/HomeReview";
 
 const Home = () => {
   useTitle("Home");
 
   return (
     <div className="lg:px-32 px-8">
-      <p>Home</p>
-
+      <MainSection></MainSection>
+      <Services></Services>
+      <Carousel></Carousel>
+      <OfferSection></OfferSection>
+      <TopSellingProducts></TopSellingProducts>
       <PageAnalysis></PageAnalysis>
+      <HomeReview></HomeReview>
+      <HomeBlog></HomeBlog>
       <HomeContact></HomeContact>
     </div>
   );
