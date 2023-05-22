25e3bfddd865130955a62962d0a476c4fec6b6cb
rehadhasan <rehadhasan664@gmail.com>
2023-05-23 02:24:54 +0600

    carousel section

--- src/Components/Home/Carousel/Carousel.js
+++ src/Components/Home/Carousel/Carousel.js
@@ -1,7 +1,59 @@
 import React from "react";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  return (
+    <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/zRZNcdt/7.webp"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/jhw2nXW/4.jpg"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/YPPVjGF/1.jpg"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/m5Z2DX5/10.webp"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/nPkJg9y/13.jpg"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/544L7mf/16.jpg"
+          className="rounded-box"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          alt=""
+          src="https://i.ibb.co/XDJXBDt/35.webp"
+          className="rounded-box"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
