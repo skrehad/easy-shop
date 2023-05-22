52da5879eeaef9da099470795916e078c6318c37
rehadhasan <rehadhasan664@gmail.com>
2023-05-22 23:51:42 +0600

    update offer section

--- src/Components/Home/OfferSection/OfferSection.js
+++ src/Components/Home/OfferSection/OfferSection.js
@@ -4,13 +4,13 @@ const OfferSection = () => {
   return (
     <div className="grid lg:grid-cols-3">
       <div>
-        <img src="" alt="" />
+        <img src="https://i.ibb.co/Yk3vPmG/4.jpg" alt="" />
       </div>
       <div>
         <p>Offer</p>
       </div>
       <div>
-        <img src="" alt="" />
+        <img src="https://i.ibb.co/Qvwz3F4/49.jpg" alt="" />
       </div>
     </div>
   );
