b78f5f65b8b030c477032e8711cc297d0bcfc52a
rehadhasan <rehadhasan664@gmail.com>
2023-05-23 17:11:32 +0600

    deleted some picture

--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <p>Services</p>
+    </div>
+  );
 };
 
 export default Services;
