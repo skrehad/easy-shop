d7bb96385181358f7c7b6037b8efa0011806aefd
rehadhasan <rehadhasan664@gmail.com>
2023-05-23 20:17:46 +0600

    complete short section

--- src/Components/Home/ShortSection/ShortSection.js
+++ src/Components/Home/ShortSection/ShortSection.js
@@ -11,7 +11,7 @@ const ShortSection = () => {
       </div>
       <div className="mt-8">
         <Link to="/products">
-          <button className="btn mb-5 text-xl border-purple-900 hover:bg-[#ff0336] hover:text-white font-mono btn-outline">
+          <button className="btn mb-5 text-xl text-white  hover:border-purple-900 bg-[#ff0336] hover:text-black font-mono btn-outline">
             Buy Now
           </button>
         </Link>
