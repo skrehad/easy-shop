56583e7f937c3b9c94f70c9524f4fc6d215d7acd
rehadhasan <rehadhasan664@gmail.com>
2023-05-23 20:08:28 +0600

    update products section

--- src/Components/Home/ShortSection/ShortSection.js
+++ src/Components/Home/ShortSection/ShortSection.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ShortSection = () => {
-  return <div>ShortSection</div>;
+  return (
+    <div>
+      <div>
+        <p className="text-3xl font-bold font-mono ">
+          We Are Always Providing Best <br /> Products Service For You
+        </p>
+      </div>
+      <div className="mt-8">
+        <Link to="/products">
+          <button className="btn mb-5 text-xl border-purple-900 hover:bg-[#ff0336] hover:text-white font-mono btn-outline">
+            Buy Now
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ShortSection;
