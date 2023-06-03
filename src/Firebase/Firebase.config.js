f1215f7135a774e3b3a000c7549584d32cce1a78
rehadhasan <rehadhasan664@gmail.com>
2023-06-03 10:54:42 +0600

    update firebase

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,18 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
-  apiKey: "AIzaSyD4P7y5A3x4fUyKgoWXwStNDXK1cXn4cFI",
-  authDomain: "easy-shop-20d6f.firebaseapp.com",
-  projectId: "easy-shop-20d6f",
-  storageBucket: "easy-shop-20d6f.appspot.com",
-  messagingSenderId: "636883023638",
-  appId: "1:636883023638:web:18fe1a952abb08f1030237",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  // apiKey: "AIzaSyD4P7y5A3x4fUyKgoWXwStNDXK1cXn4cFI",
+  // authDomain: "easy-shop-20d6f.firebaseapp.com",
+  // projectId: "easy-shop-20d6f",
+  // storageBucket: "easy-shop-20d6f.appspot.com",
+  // messagingSenderId: "636883023638",
+  // appId: "1:636883023638:web:18fe1a952abb08f1030237",
 };
 
 // Initialize Firebase
