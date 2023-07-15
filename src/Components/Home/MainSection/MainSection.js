31357e95466e7a4089ae09b53b467086c8ecc509
rehadhasan <rehadhasan664@gmail.com>
2023-07-15 10:46:12 +0600

    update Mainsection components

--- src/Components/Home/MainSection/MainSection.js
+++ src/Components/Home/MainSection/MainSection.js
@@ -4,7 +4,7 @@ import "./MainSection.css";
 const MainSection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const images = [
-    "https://i.ibb.co/8YR9vBN/1.jpg",
+    "https://i.ibb.co/LgDCHgZ/what-gaming-computer-accessories-needed-5233-1663994276529-1.webp",
     "https://i.ibb.co/pP4Pm3D/istockphoto-635790376-170667a.jpg",
     "https://i.ibb.co/hY9J7pr/Computer-Accessories-banner.jpg",
     "https://i.ibb.co/bmfqjm4/2.jpg",
